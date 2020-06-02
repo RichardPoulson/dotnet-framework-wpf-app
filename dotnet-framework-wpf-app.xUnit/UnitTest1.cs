93265644adcfc40c75e2775d551c5e0c67daf644
Richard Poulson <contact@richardpoulson.com>
2020-06-02 10:47:14 -0600

    Added .travis.yml

---
+++ dotnet-framework-wpf-app.xUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dotnet_framework_wpf_app.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
